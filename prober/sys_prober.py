79a277f39eae542dcaec761f9e35c082f86e6d18
IDNatte <idnatte@gmail.com>
2024-02-01 17:25:11 +0800

    add cpu and memory info

--- prober/sys_prober.py
+++ prober/sys_prober.py
@@ -36,59 +36,64 @@ class SystemProberThread(threading.Thread):
             else:
                 journal = []
 
-            payload = {
-                "probe_time": f"{datetime.datetime.now()}",
-                "system_info": {
-                    "net_info": [
-                        {
-                            "interface": net,
-                            "receive": network.get(net).bytes_recv,
-                            "transfer": network.get(net).bytes_sent,
-                            "ip_addr": [
-                                net.address
-                                for net in psutil.net_if_addrs().get(net)
-                                if net.family == socket.AF_INET
-                            ],
-                        }
-                        for net in network
-                    ],
-                    "hostname": socket.gethostname(),
-                },
-                "application": {
-                    "app_net_usage": [
-                        {
-                            "proc_name": psutil.Process(conn.pid).name(),
-                            "pid": conn.pid,
-                            "status": conn.status,
-                            "address": {
-                                "local_address": conn.laddr[0],
-                                "local_port": conn.laddr[1],
-                                "remote_address": conn.raddr[0],
-                                "remote_port": conn.raddr[1],
-                            },
-                        }
-                        for conn in psutil.net_connections()
-                        if conn.status == psutil.CONN_ESTABLISHED
-                    ],
-                    "running_app": [
-                        {
-                            "proc_name": proc.info.get("name"),
-                            "proc_info": proc.info,
-                            "used_resource": {
-                                "cpu": psutil.Process(
-                                    proc.info.get("pid")
-                                ).cpu_percent(),
-                                "mem": psutil.Process(
-                                    proc.info.get("pid")
-                                ).memory_percent(memtype="rss"),
-                            },
-                        }
-                        for proc in psutil.process_iter(["pid", "name", "username"])
-                    ],
-                },
-                "journal_info": journal,
-            }
-            packed = msgpack.packb(payload)
+            packed = msgpack.packb(
+                {
+                    "probe_time": f"{datetime.datetime.now()}",
+                    "system_info": {
+                        "net_info": [
+                            {
+                                "interface": net,
+                                "receive": network.get(net).bytes_recv,
+                                "transfer": network.get(net).bytes_sent,
+                                "ip_addr": [
+                                    net.address
+                                    for net in psutil.net_if_addrs().get(net)
+                                    if net.family == socket.AF_INET
+                                ],
+                            }
+                            for net in network
+                        ],
+                        "hostname": socket.gethostname(),
+                        # ToDo add cpu, memory monitoring
+                        "cpu": "",
+                        "memory": "",
+                    },
+                    "application": {
+                        "app_net_usage": [
+                            {
+                                "proc_name": psutil.Process(conn.pid).name(),
+                                "pid": conn.pid,
+                                "status": conn.status,
+                                "address": {
+                                    "local_address": conn.laddr[0],
+                                    "local_port": conn.laddr[1],
+                                    "remote_address": conn.raddr[0],
+                                    "remote_port": conn.raddr[1],
+                                },
+                            }
+                            for conn in psutil.net_connections()
+                            if conn.status == psutil.CONN_ESTABLISHED
+                        ],
+                        "running_app": [
+                            {
+                                "proc_name": proc.info.get("name"),
+                                "proc_info": proc.info,
+                                "used_resource": {
+                                    "cpu": psutil.Process(
+                                        proc.info.get("pid")
+                                    ).cpu_percent(),
+                                    "mem": psutil.Process(
+                                        proc.info.get("pid")
+                                    ).memory_percent(memtype="rss"),
+                                },
+                            }
+                            for proc in psutil.process_iter(["pid", "name", "username"])
+                        ],
+                    },
+                    "journal_info": journal,
+                }
+            )
+
             sys_probed.write(packed)
 
     def stop(self):
